292707213cafc0c4126ff55ed1f00eaed035ebee
Elena <elenalizina90@gmail.com>
2017-08-20 22:02:19 +0300

    Implemented template quick_sort method for scalar type

--- Sort/main.cpp
+++ Sort/main.cpp
@@ -1,8 +1,27 @@
 #include <QCoreApplication>
+#include <iostream>
+
+#include "quick_sort.h"
+
+template<typename T>
+void printArr(T *pAr, int size)
+{
+    for (int i = 0; i < size; i++) {
+        std::cout << pAr[i] << " ";
+    }
+    std::cout << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    const int SIZE = 8;
+    float arr[SIZE] = {2.2, 8.5, 3.0, 1.15, 7.4, 5.2, 6.9, 4.2};
+
+    printArr<float>(arr, SIZE);
+    sort<float>(arr, 0, SIZE);
+    printArr<float>(arr, SIZE);
 
     return a.exec();
 }
---
+++ Sort/quick_sort.h
@@ -0,0 +1,55 @@
+#ifndef QUICK_SORT_H
+#define QUICK_SORT_H
+
+/**
+ * @brief swap 2 elements in array
+ */
+template<typename T>
+void swap(T *pArr, int fromIndex, int toIndex)
+{
+    T tmp = pArr[fromIndex];
+    pArr[fromIndex] = pArr[toIndex];
+    pArr[toIndex] = tmp;
+}
+
+/**
+ * @brief partition - separate array to 2 subarrays
+ * @return place where was separated
+ */
+template<typename T>
+int partition(T *pArr, int fromIndex, int toIndex)
+{
+    T elem = pArr[toIndex];
+    int i = fromIndex - 1;
+
+    for (int j = fromIndex; j < toIndex; j++) {
+        if (pArr[j] < elem) {
+            ++i;
+            swap(pArr, i, j);
+        }
+    }
+
+    if (pArr[toIndex] < pArr[i + 1]) {
+        swap(pArr, i + 1, toIndex);
+    }
+
+    return i + 1;
+}
+
+/**
+ * @brief sort - ascending sort
+ * @param int* - array to sort
+ * @param int - position of start element
+ * @param int - position of last element
+ */
+template<typename T>
+void sort(T *pArr, int fromIndex, int toIndex)
+{
+    if (fromIndex < toIndex) {
+        int res = partition(pArr, fromIndex, toIndex);
+        sort(pArr, fromIndex, res - 1);
+        sort(pArr, res + 1, toIndex);
+    }
+}
+
+#endif // QUICK_SORT_H
